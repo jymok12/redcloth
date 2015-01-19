6d50345af68f460fcbc174dd4775b0f1425ca45c
Charlie Somerville <charlie@charliesomerville.com>
2015-01-19 16:55:36 +1100

    bump version to 4.2.9.github1

--- lib/redcloth/version.rb
+++ lib/redcloth/version.rb
@@ -3,23 +3,23 @@ module RedCloth
     MAJOR = 4
     MINOR = 2
     TINY  = 9
-    RELEASE_CANDIDATE = nil
+    RELEASE_CANDIDATE = "github1"
 
     STRING = [MAJOR, MINOR, TINY, RELEASE_CANDIDATE].compact.join('.')
     TAG = "REL_#{[MAJOR, MINOR, TINY, RELEASE_CANDIDATE].compact.join('_')}".upcase.gsub(/\.|-/, '_')
     FULL_VERSION = "#{[MAJOR, MINOR, TINY, RELEASE_CANDIDATE].compact.join('.')}"
-    
+
     class << self
       def to_s
         STRING
       end
-      
+
       def ==(arg)
         STRING == arg
       end
     end
   end
-  
+
   NAME = "RedCloth"
   GEM_NAME = NAME
   URL  = "http://redcloth.org/"
